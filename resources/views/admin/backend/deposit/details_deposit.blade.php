d54f93d4304d8fea48df94214bfe9fb5882c88a0
easylearningbd <elebdsoft@gmail.com>
2025-09-19 01:15:43 +0600

    99 Admin Deposit Details Page Setup Part 2

--- resources/views/admin/backend/deposit/details_deposit.blade.php
+++ resources/views/admin/backend/deposit/details_deposit.blade.php
@@ -16,7 +16,99 @@
 </header>
 
 
-<div class="row">
+<div class="row g-4">
+    <!-- Left Column  -->
+
+<div class="col-md-4">
+    <div class="card shadow-sm">
+        <div class="card-header">
+    <h5 class="mb-0"><b> {{ ucfirst($details->payment_type) }} Deposit</b></h5>
+        </div>
+
+    <div class="card-body">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item d-flex justify-content-between">
+                <span><b> Date :</b> </span>
+                <span> {{ $details->created_at->format('Y-m-d h:i A') }} </span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between">
+                <span><b> Transaction Number :</b> </span>
+                <span> {{ $details->trx }} </span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between">
+                <span><b> User Name :</b></span>
+                <span> {{ $details->user->name }} </span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between">
+                <span><b> Payment Method :</b></span>
+                <span> {{ ucfirst(str_replace('_',' ',$details->payment_type)) }} </span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between">
+                <span><b> Amount : </b></span>
+                <span> ${{ $details->amount }} </span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between">
+                <span><b> Total Amount :</b> </span>
+                <span> ${{ $details->total_amount }} </span>
+            </li>
+
+            <li class="list-group-item d-flex justify-content-between">
+                <span><b> Property Name : </b></span>
+                <span> {{ $details->property->title }} </span>
+            </li>
+
+
+             <li class="list-group-item d-flex justify-content-between">
+                <span><b> Status : </b></span>
+                <span class="badge bg-warning"> {{ ucfirst($details->status) }}  </span>
+            </li>
+    @if ($details->installment)
+    @php
+        $investment = $details->installment->investment;
+        $property = $investment->property;
+
+        $hasDownPayment = $property->down_payment > 0;
+
+        // get installment position in collection
+        $installmentIndex = $investment->installments->search(function($deposit) use ($details){
+            return $deposit->id === $details->installment->id;
+        });
+
+        $effectiveIndex = $hasDownPayment ? $installmentIndex : $installmentIndex + 1;
+
+    if ($installmentIndex === 0 && $hasDownPayment) {
+        $installmentType = 'Down Payment';
+    } else {
+        if ($effectiveIndex % 10 == 1 && $effectiveIndex % 100 != 11) {
+            $suffix = 'st';
+        } elseif ($effectiveIndex % 10 == 2 && $effectiveIndex % 100 != 12) {
+            $suffix = 'nd';
+        }elseif ($effectiveIndex % 10 == 3 && $effectiveIndex % 100 != 13) {
+            $suffix = 'rd';
+        } else {
+            $suffix = 'th';
+        }
+
+        $installmentType = $effectiveIndex . $suffix . ' Installment'; 
+    }         
+    @endphp 
+    <li class="list-group-item d-flex justify-content-between">
+        <span><b> Installment  :</b> </span>
+        <span>  {{ $installmentType }} </span>
+      </li>
+    
+    @endif
+        </ul>
+
+    </div>
+
+    </div>
+
+</div>
+
+
+
+
  
 </div>
 
