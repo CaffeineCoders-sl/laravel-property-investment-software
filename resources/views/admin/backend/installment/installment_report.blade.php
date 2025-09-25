6a39a17cf341a6950a2befcb131b3aaf1e0a8293
easylearningbd <elebdsoft@gmail.com>
2025-09-26 02:06:40 +0600

    126 Admin Installment Log Setup For Report Part 2

--- resources/views/admin/backend/installment/installment_report.blade.php
+++ resources/views/admin/backend/installment/installment_report.blade.php
@@ -15,6 +15,47 @@
     </div>
 </header>
 
+<section class="card">
+    <div class="card-body">
+        <div class="row mb-4">
+            
+      <div class="col-md-6">
+       <label for="propertyFilter" class="fw-bold">Select Property:</label>
+       <select id="propertyFilter" class="form-control">
+        <option value=""> -- All Properties -- </option>
+        @php
+            $properties = collect($installments)
+            ->flatten() // Nested groupBy Remove
+            ->map(fn($i) => $i->investment->property)
+            ->filter() 
+            ->unique('id'); // unique property id
+        @endphp
+
+        @foreach ($properties as $property)
+        <option value="{{ $property->id }}"> {{ $property->title }} </option>
+        @endforeach 
+
+       </select> 
+      </div>
+
+    <!--- User List --->
+
+    <div class="col-md-6" id="userFilterWrapper" style="display: none">
+        <label for="userFilter" class="fw-bold">Select Investor/User:</label>
+        <select id="userFilter">
+            <option value=""> -- Select User --</option> 
+        </select>
+    </div>
+
+
+
+
+
+        </div> 
+    </div>
+
+</section>
+
 
 
 
